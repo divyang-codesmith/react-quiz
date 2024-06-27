6a72a4c5bce608a2a5b60dc2ba194ee81b3543bc
Divyang Patel <divyangpatel54@gmail.com>
2024-06-27 08:40:41 +0530

    added correct option code

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,20 @@
-function Options({ options }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer !== null;
   return (
     <div className='options'>
-      {options.map((option) => (
-        <button className='btn btn-option' key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+            hasAnswer
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }`}
+          key={option}
+          disabled={hasAnswer}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+        >
           {option}
         </button>
       ))}
